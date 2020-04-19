c7e971af84dc9393bb2749b24af669e57af8b5fd
Janne Suotsalo <janne.suotsalo@gmail.com>
2020-04-19 07:05:48 +0300

    done part 1

--- js/main.js
+++ js/main.js
@@ -1,31 +1,11 @@
-"use strict";
-
 window.addEventListener("load", async () => {
-  const ul = document.querySelector("ul");
-  const rfrsh = document.querySelector("#refresh");
-  const form = document.querySelector("form");
-  const username = "Janne";
-  const greeting = form.elements.greeting;
-  console.log("hello");
+  "use strict";
 
-  const init = async () => {
-    const data = [];
+  if ("serviceWorker" in navigator) {
     try {
-      const greetings = await getGreetingsByUser(username);
-      for (const message of greetings) {
-        data.push(message);
-      }
+      await navigator.serviceWorker.register("./sw.js");
     } catch (e) {
       console.log(e.message);
     }
-
-    ul.innerHTML = "";
-    data.forEach((item) => {
-      ul.innerHTML += `<ul>${item.username}: ${item.greeting}</ul>`;
-    });
-  };
-
-  init();
-
-  rfrsh.addEventListener("click", init);
+  }
 });
--- sw.js
+++ sw.js
@@ -1,42 +1,46 @@
-'use strict';
-self.importScripts('./js/fetchGQL.js');
-self.importScripts('./js/idb.js');
-const cacheName = 'hello-pwa';
+"use strict";
+const cacheName = "hello-pwa";
 const filesToCache = [
-  './',
-  './index.html',
-  './favicon.ico',
-  './css/style.css',
-  './js/main.js',
-  './js/idb.js',
-  './images/pwa.png',
+  "./",
+  "./index.html",
+  "./favicon.ico",
+  "./css/style.css",
+  "./js/main.js",
+  "./images/Untitled.jpg",
+  "./acme-v9-latin/acme-v9-latin-regular.eot",
+  "./acme-v9-latin/acme-v9-latin-regular.svg",
+  "./acme-v9-latin/acme-v9-latin-regular.ttf",
+  "./acme-v9-latin/acme-v9-latin-regular.woff",
+  "./acme-v9-latin/acme-v9-latin-regular.woff2",
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', (e) => {
-  e.waitUntil((async () => {
-    try {
-      const cache = await caches.open(cacheName);
-      // console.log(cache);
-      return cache.addAll(filesToCache);
-    }
-    catch (e) {
-      console.log('after install', e.message);
-    }
-  })());
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    (async () => {
+      try {
+        const cache = await caches.open(cacheName);
+        // console.log(cache);
+        return cache.addAll(filesToCache);
+      } catch (e) {
+        console.log("after install", e.message);
+      }
+    })()
+  );
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', (e) => {
+self.addEventListener("fetch", (e) => {
   // console.log(e.request);
-  e.respondWith((async () => {
-    try {
-      const response = await caches.match(e.request);
-      // console.log('resp', response);
-      return response || fetch(e.request);
-    }
-    catch (e) {
-      console.log('load cache', e.message);
-    }
-  })());
+  e.respondWith(
+    (async () => {
+      try {
+        const response = await caches.match(e.request);
+        // console.log('resp', response);
+        return response || fetch(e.request);
+      } catch (e) {
+        console.log("load cache", e.message);
+      }
+    })()
+  );
 });
